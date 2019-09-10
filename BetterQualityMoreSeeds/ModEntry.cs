874ac7458bbf8bdc6205858c9833ad17bed3acdb
Brandon Marquis Markail Green <bmarquismarkail@github.com>
2019-09-10 14:34:13 -0500

    Removing needless files Slight update \

--- BetterQualityMoreSeeds/ModEntry.cs
+++ BetterQualityMoreSeeds/ModEntry.cs
@@ -16,42 +16,46 @@ namespace SB_BQMS
     {
         Dictionary<StardewValley.Object, AllSeedMakerValueContainer> allSeedMakers;
         Dictionary<StardewValley.Object, AllChestsValueContainer> allChests;
-
-        StardewValley.Object previousHeldItem;
-
         GameLocation previousLocation;
         bool isInitialized;
         bool hasAutomate;
+
+        public StardewValley.Object PreviousHeldItem { get; set; }
+        public Dictionary<StardewValley.Object, AllChestsValueContainer> AllChests { get => allChests; set => allChests = value; }
+
         /*********
-        ** Public methods
-        *********/
+** Public methods
+*********/
         /// <summary>Initialise the mod.</summary>
         /// <param name="helper">Provides methods for interacting with the mod directory, such as read/writing a config file or custom JSON files.</param>
         public override void Entry(IModHelper helper)
         {
             if (Helper.ModRegistry.IsLoaded("Pathoschild.Automate")) hasAutomate = true; else hasAutomate = false;
 
-            Helper.Events.GameLoop.SaveLoaded += initializeMod;
-            Helper.Events.GameLoop.ReturnedToTitle += resetMod;
+            Helper.Events.GameLoop.SaveLoaded += InitializeMod;
+            Helper.Events.GameLoop.ReturnedToTitle += ResetMod;
             Helper.Events.GameLoop.UpdateTicked += ModUpdate;
-            Helper.Events.World.ObjectListChanged += populateSeedmakers;
+            Helper.Events.World.ObjectListChanged += PopulateSeedmakers;
             allSeedMakers = new Dictionary<StardewValley.Object, AllSeedMakerValueContainer>();
-            if (hasAutomate) allChests = new Dictionary<StardewValley.Object, AllChestsValueContainer>();
+            if (hasAutomate) AllChests = new Dictionary<StardewValley.Object, AllChestsValueContainer>();
             isInitialized = false;
         }
 
         //  For Automate  We now need to get all seed makers available.
         //  So making this a method on its own
-        private void populateSeedmakersBase()
+        private void PopulateSeedmakersBase()
         {
             allSeedMakers.Clear();
-            if (hasAutomate) allChests.Clear();
+            if (hasAutomate) AllChests.Clear();
 
-            foreach (GameLocation location in Game1.locations)
+            for (int i = 0; i < Game1.locations.Count; i++)
             {
+                GameLocation location = Game1.locations[i];
                 OverlaidDictionary allObjects = location.objects;
-                    foreach (StardewValley.Object ob in allObjects.Values)
+                for (var j = allObjects.Values.GetEnumerator(); j.MoveNext();)
                 {
+                    StardewValley.Object ob = j.Current;
+                    if (ob.Equals(null)) return;
                     if (ob.name.Equals("Seed Maker"))
                     {
                         if (!allSeedMakers.ContainsKey(ob))
@@ -59,21 +63,20 @@ namespace SB_BQMS
                     }
                     if (hasAutomate)
                     {
-                        if ((ob is Chest) && (ob as Chest).playerChest)
+                        if ((ob is Chest) && (ob as Chest).playerChest.Value)
                         {
-                            if (!allChests.ContainsKey(ob))
-                                allChests.Add(ob, new AllChestsValueContainer(null, location, false));
+                            if (!AllChests.ContainsKey(ob))
+                                AllChests.Add(ob, new AllChestsValueContainer(null, location, false));
                         }
                     }
                 }
-
             }
             previousLocation = Game1.player.currentLocation;
         }
 
-        private void populateSeedmakers(object sender, ObjectListChangedEventArgs e)
+        private void PopulateSeedmakers(object sender, ObjectListChangedEventArgs e)
         {
-            populateSeedmakersBase();
+            PopulateSeedmakersBase();
         }
 
         /*********
@@ -82,22 +85,22 @@ namespace SB_BQMS
         /// <summary>Initializes the Mod. Adds all Seed Makers in the Game
         /// <param name="sender">The event sender.</param>
         /// <param name="e">The event data.</param>
-        private void initializeMod(object sender, EventArgs e)
+        private void InitializeMod(object sender, EventArgs e)
         {
             previousLocation = Game1.player.currentLocation;
             //TODO: Add the allSeedMakers population code here.
             //TODO: add a LocationObjectsChanged method to check if the allSeedMakers variable needs to be updated
             //Note: The Method has been designed to do this since alpha! LOL
-            populateSeedmakersBase();
+            PopulateSeedmakersBase();
             isInitialized = true;
         }
 
-        private void resetMod(object sender, EventArgs e)
+        private void ResetMod(object sender, EventArgs e)
         {
             isInitialized = false;
         }
 
-        private StardewValley.Object checkChests(StardewValley.Object seedmaker)
+        private StardewValley.Object CheckChests(StardewValley.Object seedmaker)
         {
             OverlaidDictionary allObjectsinLocation = allSeedMakers[seedmaker].location.objects;
             OverlaidDictionary.KeysCollection keyCollection = allObjectsinLocation.Keys;
@@ -116,16 +119,16 @@ namespace SB_BQMS
                             {
                                 if (Convert.ToInt32(keyValuePair.Value.Split('/')[3]) == currentItems[index].ParentSheetIndex)
                                 {
-                                    allChests[thisChest].previousItem = currentItems[index];
+                                    AllChests[thisChest].previousItem = currentItems[index];
                                     return currentItems[index] as StardewValley.Object;
                                 }
                             }
                         }
                     }
-                    if (allChests[thisChest].previousItem != null)
+                    if (AllChests[thisChest].previousItem != null)
                     {
-                        Item returnItem = allChests[thisChest].previousItem;
-                        allChests[thisChest].previousItem = null;
+                        Item returnItem = AllChests[thisChest].previousItem;
+                        AllChests[thisChest].previousItem = null;
                         return returnItem as StardewValley.Object;
                     }
                 }
@@ -138,40 +141,45 @@ namespace SB_BQMS
             if (isInitialized)
             {
                 List<StardewValley.Object> seedMakers = allSeedMakers.Keys.ToList();
-                foreach (StardewValley.Object seedMaker in seedMakers)
+
+                for (int i = 0; i < seedMakers.Count; i++)
                 {
-                    if (seedMaker.heldObject == null && allSeedMakers[seedMaker].hasBeenChecked == true)
+                    StardewValley.Object seedMaker = seedMakers[i];
+                    if (seedMaker.heldObject.Equals(null))
                     {
-                        allSeedMakers[seedMaker].droppedObject = null;
-                        allSeedMakers[seedMaker].hasBeenChecked = false;
-                    }
-                    if (seedMaker.heldObject != null && allSeedMakers[seedMaker].hasBeenChecked == false && allSeedMakers[seedMaker].droppedObject == null)
-                    {
-                        if (hasAutomate)
+                        if (allSeedMakers[seedMaker].hasBeenChecked.Equals(true))
                         {
-                            // This should invoke checkChests, which will scan the seed maker for adjacent chests,
-                            // Then checks if those chests inventory decreased.
-                            StardewValley.Object droppedChestObject = checkChests(seedMaker);
-                            if (droppedChestObject != null)
+                            allSeedMakers[seedMaker].droppedObject = null;
+                            allSeedMakers[seedMaker].hasBeenChecked = false;
+                        }
+                        if (allSeedMakers[seedMaker].hasBeenChecked.Equals(false) && allSeedMakers[seedMaker].droppedObject == null)
+                        {
+                            if (hasAutomate)
                             {
-                                allSeedMakers[seedMaker].droppedObject = droppedChestObject;
-                                seedMaker.addToStack(allSeedMakers[seedMaker].droppedObject.Quality == 4 ? allSeedMakers[seedMaker].droppedObject.Quality - 1 : allSeedMakers[seedMaker].droppedObject.Quality);
-                                allSeedMakers[seedMaker].hasBeenChecked = true;
-                                continue;
+                                // This should invoke checkChests, which will scan the seed maker for adjacent chests,
+                                // Then checks if those chests inventory decreased.
+                                StardewValley.Object droppedChestObject = CheckChests(seedMaker);
+                                if (droppedChestObject != null)
+                                {
+                                    allSeedMakers[seedMaker].droppedObject = droppedChestObject;
+                                    seedMaker.addToStack(allSeedMakers[seedMaker].droppedObject.Quality == 4 ? allSeedMakers[seedMaker].droppedObject.Quality - 1 : allSeedMakers[seedMaker].droppedObject.Quality);
+                                    allSeedMakers[seedMaker].hasBeenChecked = true;
+                                    continue;
+                                }
                             }
                         }
                         //  Checks if the Farmer is in the same location as the seed maker
                         //  This will save up cpu time for the ones that are not on location
-                        if (previousHeldItem != null && Game1.player.currentLocation == allSeedMakers[seedMaker].location)
+                        if (PreviousHeldItem != null && Game1.player.currentLocation == allSeedMakers[seedMaker].location)
                         {
-                            allSeedMakers[seedMaker].droppedObject = previousHeldItem;
+                            allSeedMakers[seedMaker].droppedObject = PreviousHeldItem;
                             seedMaker.addToStack(allSeedMakers[seedMaker].droppedObject.Quality == 4 ? allSeedMakers[seedMaker].droppedObject.Quality - 1 : allSeedMakers[seedMaker].droppedObject.Quality);
                             allSeedMakers[seedMaker].hasBeenChecked = true;
                             continue;
                         }
                     }
                 }
-                previousHeldItem = Game1.player.ActiveObject;
+                PreviousHeldItem = Game1.player.ActiveObject;
             }
         }
     }
